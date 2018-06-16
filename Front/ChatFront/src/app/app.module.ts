b7eca883d0865efb4f6667a29ebf9c743777b432
mena <mena.emile@gmail.com>
2018-06-16 17:41:58 -0500

    routes changes

--- Front/ChatFront/src/app/app.module.ts
+++ Front/ChatFront/src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { myRoutes } from './app-routes';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    myRoutes
   ],
   providers: [],
   bootstrap: [AppComponent]
