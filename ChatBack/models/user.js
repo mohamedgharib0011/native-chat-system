3d8b8fbb01402c7c4d99e70eb9bf6f8ebfacabd6
Mohamed Gharib <m.beshirfci@gmail.com>
2018-06-20 16:08:59 +0200

    Adding signup link at login screen and vice versa, remove hashed code

--- ChatBack/models/user.js
+++ ChatBack/models/user.js
@@ -4,8 +4,7 @@
  * @Last Modified time: 2018-06-17 22:28:58 
  */
 
-const mongoose = require('mongoose'),
-    bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
 
 
 
@@ -20,26 +19,5 @@ const userSchema = new mongoose.Schema({
 
 // using fulltext index for searching name and email instead of using regex
 userSchema.index({ name: 'text', email: 'text' });
-
-// userSchema.pre('save',(next)=>{
-//      // only hash the password if it has been modified (or is new)
-//     //  if (!this.isModified('password')) return next();
-    
-//     console.log("*********** this: "+Object.values(this));
-    
-//     // generate a salt
-//     bcrypt.genSalt(10, function(err, salt) {
-//         if (err) return next(err);
-//         console.log("********** password: "+this.password+" salt: "+salt);
-//         // hash the password along with our new salt
-//         bcrypt.hash(this.password, salt, function(err, hash) {
-//             if (err) return next(err);
-
-//             // override the cleartext password with the hashed one
-//             this.password = hash;
-//             next();
-//         });
-//     });
-// })
 
 module.exports = mongoose.model('User', userSchema);
